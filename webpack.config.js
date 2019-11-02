422da606ba9a8a018911baf3b14a69336b79bc69
Susie Sun <sunjiaping1992@gmail.com>
2019-11-02 16:45:38 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+// entry point --> output
+
+//return /Users/Susie_SUN/Desktop/Language/Web Devlopment/React-front-end/indecision-app'
+//console.log(__dirname);
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/, // support both scss and css file
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
+  }
+};
+
+
+
+
